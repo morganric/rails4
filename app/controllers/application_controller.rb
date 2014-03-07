a1aade4396d53887f1e0781e283076cd8466d022
morganric <morganric@gmail.com>
2014-03-07 22:09:56 +0000

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,25 +3,25 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def fb_liked?
-    session[:signed_request]['page']['liked'] if session[:signed_request]
-  end
+  # def fb_liked?
+  #   session[:signed_request]['page']['liked'] if session[:signed_request]
+  # end
  
-  def fb_admin?
-    session[:signed_request]['page']['admin'] if session[:signed_request]
-	  end
+  # def fb_admin?
+  #   session[:signed_request]['page']['admin'] if session[:signed_request]
+	 #  end
 		
-  def fb_page?
-    session[:signed_request]['page']['id'] if session[:signed_request]
-  end	
+  # def fb_page?
+  #   session[:signed_request]['page']['id'] if session[:signed_request]
+  # end	
 
-  helper_method :fb_liked?, :fb_admin?, :fb_page?
+  # helper_method :fb_liked?, :fb_admin?, :fb_page?
  
-  before_filter do
-    if params[:signed_request]
-      oauth = Koala::Facebook::OAuth.new(ENV['FB_APP_ID'], ENV['FB_APP_SECRET'])
-      session[:signed_request] = oauth.parse_signed_request(params[:signed_request])
-    end
-  end
+  # before_filter do
+  #   if params[:signed_request]
+  #     oauth = Koala::Facebook::OAuth.new(ENV['FB_APP_ID'], ENV['FB_APP_SECRET'])
+  #     session[:signed_request] = oauth.parse_signed_request(params[:signed_request])
+  #   end
+  # end
 
 end
